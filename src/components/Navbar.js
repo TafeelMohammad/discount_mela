381ba83b70650d3dc13b77ec0bfa1a90cb17b4d4
TafeelMohammad <86819725+TafeelMohammad@users.noreply.github.com>
2025-05-22 08:14:21 +0000

    nav bar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "../assets/images/discount-mela-logo.png";
 
 const Navbar = () => {
   return React.createElement(
     "nav",
     { className: "navbar" },
     React.createElement(
-      React.Fragment,
-      null,
+      "div",
+      { className: "nav-left" },
       React.createElement(Link, { className: "nav-link", to: "/signup" }, "Signup"),
       React.createElement("span", { className: "separator" }, " | "),
       React.createElement(Link, { className: "nav-link", to: "/login" }, "Login")
+    ),
+    React.createElement(
+      "div",
+      { className: "nav-center" },
+      React.createElement("img", {
+        src: logo,
+        alt: "Discount Mela Logo",
+        className: "logo"
+      })
+    ),
+    React.createElement(
+      "div",
+      { className: "nav-right" },
+      React.createElement("span", { className: "nav-text" }, "Welcome to Discount Mela!")
     )
   );
 };
